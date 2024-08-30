8e1c4bb1aec0c09544eb60ae47b1eefe143367ef
mr nim <mrnim@nimtechnology.com>
2024-08-30 11:51:34 +0700

    Specify the sizw of each chunk of a file to be downloaded from S3

--- helper/aws_cloud/aws_cloud_impl/s3_impl.go
+++ helper/aws_cloud/aws_cloud_impl/s3_impl.go
@@ -3,10 +3,14 @@ package aws_cloud_impl
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
 	"jougan/log"
+	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
@@ -58,23 +62,106 @@ func (ac *AWSConfiguration) DownloadS3FileToMemory(bucket string, key string) ([
 	// Create an S3 client
 	client := s3.NewFromConfig(cfg)
 
-	// Get the object from S3
-	getObjectInput := &s3.GetObjectInput{
+	// Get file size
+	headObjectOutput, err := client.HeadObject(context.TODO(), &s3.HeadObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
-	}
-	result, err := client.GetObject(context.TODO(), getObjectInput)
+	})
 	if err != nil {
-		log.Errorf("unable to download item from bucket %q, %v", bucket, err)
 		return nil, 0, err
 	}
-	defer result.Body.Close()
+	fileSize := *headObjectOutput.ContentLength
 
-	buf := new(bytes.Buffer)
-	n, err := io.Copy(buf, result.Body)
-	if err != nil {
-		log.Errorf("unable to read object into buffer, %v", err)
-		return nil, 0, err
+	// Check for PART_SIZE environment variable
+	partSizeStr := os.Getenv("PART_SIZE_MB")
+	var partSize int64
+	if partSizeStr != "" {
+		partSizeMB, err := strconv.ParseInt(partSizeStr, 10, 64)
+		if err != nil {
+			log.Errorf("Invalid PART_SIZE value: %v", err)
+			return nil, 0, err
+		}
+		partSize = partSizeMB * 1024 * 1024 // Convert MB to bytes
 	}
-	return buf.Bytes(), n, nil
+
+	// If partSize is not set, download the entire file in one go
+	if partSize <= 0 {
+		getObjectInput := &s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		}
+		result, err := client.GetObject(context.TODO(), getObjectInput)
+		if err != nil {
+			log.Errorf("unable to download item from bucket %q, %v", bucket, err)
+			return nil, 0, err
+		}
+		defer result.Body.Close()
+
+		buf := new(bytes.Buffer)
+		n, err := io.Copy(buf, result.Body)
+		if err != nil {
+			log.Errorf("unable to read object into buffer, %v", err)
+			return nil, 0, err
+		}
+		return buf.Bytes(), n, nil
+	}
+
+	// Calculate the number of parts
+	numParts := (fileSize + partSize - 1) / partSize
+
+	// Create a buffer to store the file content
+	buf := make([]byte, fileSize)
+
+	// Download parts concurrently
+	var wg sync.WaitGroup
+	errCh := make(chan error, numParts)
+
+	for i := int64(0); i < numParts; i++ {
+		start := i * partSize
+		end := start + partSize - 1
+		if end > fileSize-1 {
+			end = fileSize - 1
+		}
+
+		wg.Add(1)
+		go func(start, end int64) {
+			defer wg.Done()
+
+			getObjectInput := &s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    aws.String(key),
+				Range:  aws.String(fmt.Sprintf("bytes=%d-%d", start, end)),
+			}
+
+			result, err := client.GetObject(context.TODO(), getObjectInput)
+			if err != nil {
+				log.Errorf("unable to download part from bucket %q, %v", bucket, err)
+				errCh <- err
+				return
+			}
+			defer result.Body.Close()
+
+			// Read the part into the buffer at the correct offset
+			partBuffer := make([]byte, end-start+1)
+			_, err = io.ReadFull(result.Body, partBuffer)
+			if err != nil {
+				log.Errorf("unable to read object part into buffer, %v", err)
+				errCh <- err
+				return
+			}
+
+			// Copy the part into the correct position in the buffer
+			copy(buf[start:], partBuffer)
+		}(start, end)
+	}
+
+	// Wait for all parts to be downloaded
+	wg.Wait()
+	close(errCh)
+
+	if len(errCh) > 0 {
+		return nil, 0, <-errCh // return the first error encountered
+	}
+
+	return buf, fileSize, nil
 }
